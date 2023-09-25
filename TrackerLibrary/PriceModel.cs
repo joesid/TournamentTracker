82124c38e2437aea727d88ea6b917a7d85dbb534
joesid <joesidious@gmail.com>
2023-09-25 20:54:19 +0100

    Add project files.

---
+++ TrackerLibrary/PriceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PriceModel
+    {
+        public int PlaceNumber { get; set; }
+        public int PlaceName { get; set; }
+        public decimal PriceAmount { get; set; }
+        public double PricePercentage { get; set; }
+    }
+}
